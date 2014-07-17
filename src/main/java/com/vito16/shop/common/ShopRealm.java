4a9889af9587c4684d752c5da8b9131a756e5765
Vito Zhou <zhouwentao16@gmail.com>
2014-07-17 21:57:18 +0800

    添加Shiro依赖以及导入Shiro Taglib标签库

---
+++ src/main/java/com/vito16/shop/common/ShopRealm.java
@@ -0,0 +1,26 @@
+package com.vito16.shop.common;
+
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.realm.Realm;
+
+/**
+ * Created by Vito on 2014/7/17.
+ */
+public class ShopRealm implements Realm {
+    @Override
+    public String getName() {
+        return "shopRealm";
+    }
+
+    @Override
+    public boolean supports(AuthenticationToken token) {
+        return false;
+    }
+
+    @Override
+    public AuthenticationInfo getAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
+        return null;
+    }
+}
---
+++ src/test/java/com/vito16/shop/test/BaseTest.java
@@ -0,0 +1,44 @@
+package com.vito16.shop.test;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.config.IniSecurityManagerFactory;
+import org.apache.shiro.mgt.*;
+import org.apache.shiro.subject.Subject;
+import org.apache.shiro.util.Factory;
+import org.apache.shiro.util.ThreadContext;
+import org.junit.After;
+
+/**
+ * Created by Vito on 2014/7/17.
+ */
+public abstract class BaseTest {
+    @After
+    public void tearDown() throws Exception {
+        ThreadContext.unbindSubject();//退出时请解除绑定Subject到线程 否则对下次测试造成影响
+    }
+
+    protected void login(String configFile, String username, String password) {
+        //1、获取SecurityManager工厂，此处使用Ini配置文件初始化SecurityManager
+        Factory<org.apache.shiro.mgt.SecurityManager> factory =
+                new IniSecurityManagerFactory(configFile);
+
+        //2、得到SecurityManager实例 并绑定给SecurityUtils
+        org.apache.shiro.mgt.SecurityManager securityManager = factory.getInstance();
+        SecurityUtils.setSecurityManager(securityManager);
+
+        //3、得到Subject及创建用户名/密码身份验证Token（即用户身份/凭证）
+        Subject subject = SecurityUtils.getSubject();
+        UsernamePasswordToken token = new UsernamePasswordToken(username, password);
+
+        subject.login(token);
+    }
+
+    protected void login(String configFile) {
+        login(configFile, null, null);
+    }
+
+    public Subject subject() {
+        return SecurityUtils.getSubject();
+    }
+}
