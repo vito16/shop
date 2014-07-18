b35e8e95b504911644433e16626b55041ab34b89
Vito Zhou <zhouwentao16@gmail.com>
2014-07-18 11:59:48 +0800

    集成Shiro

--- src/main/java/com/vito16/shop/common/ShopRealm.java
+++ src/main/java/com/vito16/shop/common/ShopRealm.java
@@ -1,26 +1,78 @@
 package com.vito16.shop.common;
 
-import org.apache.shiro.authc.AuthenticationException;
-import org.apache.shiro.authc.AuthenticationInfo;
-import org.apache.shiro.authc.AuthenticationToken;
-import org.apache.shiro.realm.Realm;
+import com.vito16.shop.model.User;
+import com.vito16.shop.service.UserService;
+import org.apache.shiro.authc.*;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.authz.SimpleAuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+import org.apache.shiro.util.ByteSource;
+import org.springframework.beans.factory.annotation.Autowired;
 
 /**
- * Created by Vito on 2014/7/17.
+ * realm
+ * @author Vito
+ * @version  1.0
  */
-public class ShopRealm implements Realm {
+public class ShopRealm extends AuthorizingRealm {
+
+    @Autowired
+    private UserService userService;
+
     @Override
-    public String getName() {
-        return "shopRealm";
+    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principals) {
+        String username = (String) principals.getPrimaryPrincipal();
+        SimpleAuthorizationInfo authorizationInfo = new SimpleAuthorizationInfo();
+        //暂时不加权限
+        return authorizationInfo;
     }
 
     @Override
-    public boolean supports(AuthenticationToken token) {
-        return false;
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
+
+        String username = (String) token.getPrincipal();
+        User user = userService.findByUsernameAndPassword(username,"");
+        if (user == null) {
+            throw new UnknownAccountException();//没找到帐号
+        }
+
+        //交给AuthenticatingRealm使用CredentialsMatcher进行密码匹配，如果觉得人家的不好可以自定义实现
+        SimpleAuthenticationInfo authenticationInfo = new SimpleAuthenticationInfo(
+                user.getUsername(), //用户名
+                user.getPassword(), //密码
+//                ByteSource.Util.bytes(user.getCredentialsSalt()),//salt=username+salt
+                ByteSource.Util.bytes(user.getPassword()),
+                getName()  //realm name
+        );
+        return authenticationInfo;
     }
 
     @Override
-    public AuthenticationInfo getAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
-        return null;
+    public void clearCachedAuthorizationInfo(PrincipalCollection principals) {
+        super.clearCachedAuthorizationInfo(principals);
+    }
+
+    @Override
+    public void clearCachedAuthenticationInfo(PrincipalCollection principals) {
+        super.clearCachedAuthenticationInfo(principals);
+    }
+
+    @Override
+    public void clearCache(PrincipalCollection principals) {
+        super.clearCache(principals);
+    }
+
+    public void clearAllCachedAuthorizationInfo() {
+        getAuthorizationCache().clear();
+    }
+
+    public void clearAllCachedAuthenticationInfo() {
+        getAuthenticationCache().clear();
+    }
+
+    public void clearAllCache() {
+        clearAllCachedAuthenticationInfo();
+        clearAllCachedAuthorizationInfo();
     }
 }
