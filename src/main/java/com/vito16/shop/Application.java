ff8745dbc3aa9f32e9da2f0c0fb6512a1d7c8d2b
Vito Zhou <zhouwentao16@gmail.com>
2016-01-18 11:22:03 +0800

    修改配置文件

--- src/main/java/com/vito16/shop/Application.java
+++ src/main/java/com/vito16/shop/Application.java
@@ -9,41 +9,73 @@
  */
 package com.vito16.shop;
 
+import java.util.Properties;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
-import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+import com.alibaba.druid.pool.DruidDataSource;
 
 /**
  * @author 木鱼 muyu@yiji.com
  * @version 2015/10/30
  */
 @SpringBootApplication
-public class Application extends WebMvcConfigurerAdapter{
-
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
-
-    @Bean
-    InternalResourceViewResolver internalResourceViewResolver () {
-        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
-        viewResolver.setPrefix("/WEB-INF/views/");
-        viewResolver.setSuffix(".jsp");
-        return viewResolver;
-    }
-
-    @Override
-    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
-        configurer.enable();
-    }
-
-    @Override
-    public void configureViewResolvers(ViewResolverRegistry registry) {
-        super.configureViewResolvers(registry);
-        registry.viewResolver(internalResourceViewResolver());
-    }
+@ImportResource("classpath*:/web.xml")
+public class Application extends SpringBootServletInitializer {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+	
+	@Bean
+	public DruidDataSource dataSource() {
+		DruidDataSource dataSource = new DruidDataSource();
+		Properties prop = new Properties();
+		prop.setProperty("driverClassName", "com.mysql.jdbc.Driver");
+		prop.setProperty("url", "jdbc:mysq://localhost:3306/shop");
+		prop.setProperty("username", "root");
+		prop.setProperty("password", "root");
+		dataSource.setConnectProperties(prop);
+		
+		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+		dataSource.setUrl("jdbc:mysql://localhost:3306/shop");
+		dataSource.setUsername("root");
+		dataSource.setPassword("root");
+		return dataSource;
+	}
+	
+	@Bean
+	public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource) {
+		LocalContainerEntityManagerFactoryBean entityManager = new LocalContainerEntityManagerFactoryBean();
+		entityManager.setDataSource(dataSource);
+		entityManager.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+		entityManager.setPackagesToScan("com.vito16.shop.model");
+		Properties prop = new Properties();
+		prop.setProperty("hibernate.hbm2ddl.auto", "update");
+		prop.setProperty("hibernate.show_sql", "true");
+		prop.setProperty("hibernate.format_sql", "true");
+		prop.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5InnoDBDialect");
+		prop.setProperty("hibernate.cache.region.factory_class", "org.hibernate.cache.ehcache.EhCacheRegionFactory");
+		prop.setProperty("net.sf.ehcache.configurationResourceName", "ehcache/ehcache-hibernate-local.xml");
+		prop.setProperty("hibernate.physical_naming_strategy", "org.hibernate.cfg.ImprovedNamingStrategy");
+		entityManager.setJpaProperties(prop);
+		return entityManager;
+	}
+	
+	@Bean
+	public JpaTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+		JpaTransactionManager transactionManager = new JpaTransactionManager(entityManagerFactory);
+		return transactionManager;
+	}
+	
 }
