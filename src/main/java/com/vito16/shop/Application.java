d189a55328b09d1fcfccd39f01cb4de81d588d8f
Vito Zhou <zhouwentao16@gmail.com>
2016-03-11 11:21:37 +0800

    修改DB相关配置

--- src/main/java/com/vito16/shop/Application.java
+++ src/main/java/com/vito16/shop/Application.java
@@ -1,7 +1,12 @@
 package com.vito16.shop;
 
+import com.alibaba.druid.pool.DruidDataSource;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
 
 /**
  *
@@ -13,4 +18,40 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class);
     }
+
+    @Value("${jdbc.driverClassName}")
+    private String driverClassName;
+    @Value("${jdbc.url}")
+    private String url;
+    @Value("${jdbc.username}")
+    private String username;
+    @Value("${jdbc.password}")
+    private String password;
+
+    @Bean
+    public DataSource getDataSource(){
+        DruidDataSource druidDataSource = new DruidDataSource();
+        druidDataSource.setDriverClassName(driverClassName);
+        druidDataSource.setUrl(url);
+        druidDataSource.setUsername(username);
+        druidDataSource.setPassword(password);
+
+        druidDataSource.setMaxActive(20);
+        druidDataSource.setInitialSize(1);
+        druidDataSource.setMaxWait(60000);
+        druidDataSource.setMinIdle(1);
+
+        druidDataSource.setTimeBetweenEvictionRunsMillis(60000);
+        druidDataSource.setMinEvictableIdleTimeMillis(300000);
+
+        druidDataSource.setValidationQuery("SELECT 'x'");
+        druidDataSource.setTestWhileIdle(true);
+        druidDataSource.setTestOnBorrow(false);
+        druidDataSource.setTestOnReturn(false);
+        druidDataSource.setPoolPreparedStatements(true);
+        druidDataSource.setMaxOpenPreparedStatements(20);
+        return druidDataSource;
+    }
+
+
 }
