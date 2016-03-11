6871c3a581618b44832bd7b336701e5e5ea16b2a
Vito Zhou <zhouwentao16@gmail.com>
2016-03-11 11:37:56 +0800

    修改JPA相关配置，增加自动生成表

--- src/main/java/com/vito16/shop/Application.java
+++ src/main/java/com/vito16/shop/Application.java
@@ -5,8 +5,12 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 
+import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
+import java.util.Properties;
 
 /**
  *
@@ -14,44 +18,58 @@ import javax.sql.DataSource;
  */
 @SpringBootApplication
 public class Application {
-
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class);
-    }
-
-    @Value("${jdbc.driverClassName}")
-    private String driverClassName;
-    @Value("${jdbc.url}")
-    private String url;
-    @Value("${jdbc.username}")
-    private String username;
-    @Value("${jdbc.password}")
-    private String password;
-
-    @Bean
-    public DataSource getDataSource(){
-        DruidDataSource druidDataSource = new DruidDataSource();
-        druidDataSource.setDriverClassName(driverClassName);
-        druidDataSource.setUrl(url);
-        druidDataSource.setUsername(username);
-        druidDataSource.setPassword(password);
-
-        druidDataSource.setMaxActive(20);
-        druidDataSource.setInitialSize(1);
-        druidDataSource.setMaxWait(60000);
-        druidDataSource.setMinIdle(1);
-
-        druidDataSource.setTimeBetweenEvictionRunsMillis(60000);
-        druidDataSource.setMinEvictableIdleTimeMillis(300000);
-
-        druidDataSource.setValidationQuery("SELECT 'x'");
-        druidDataSource.setTestWhileIdle(true);
-        druidDataSource.setTestOnBorrow(false);
-        druidDataSource.setTestOnReturn(false);
-        druidDataSource.setPoolPreparedStatements(true);
-        druidDataSource.setMaxOpenPreparedStatements(20);
-        return druidDataSource;
-    }
-
+	
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class);
+	}
+	
+	@Value("${jdbc.driverClassName}")
+	private String driverClassName;
+	@Value("${jdbc.url}")
+	private String url;
+	@Value("${jdbc.username}")
+	private String username;
+	@Value("${jdbc.password}")
+	private String password;
+	
+	@Bean
+	public DataSource getDataSource() {
+		DruidDataSource druidDataSource = new DruidDataSource();
+		druidDataSource.setDriverClassName(driverClassName);
+		druidDataSource.setUrl(url);
+		druidDataSource.setUsername(username);
+		druidDataSource.setPassword(password);
+		
+		druidDataSource.setMaxActive(20);
+		druidDataSource.setInitialSize(1);
+		druidDataSource.setMaxWait(60000);
+		druidDataSource.setMinIdle(1);
+		
+		druidDataSource.setTimeBetweenEvictionRunsMillis(60000);
+		druidDataSource.setMinEvictableIdleTimeMillis(300000);
+		
+		druidDataSource.setValidationQuery("SELECT 'x'");
+		druidDataSource.setTestWhileIdle(true);
+		druidDataSource.setTestOnBorrow(false);
+		druidDataSource.setTestOnReturn(false);
+		druidDataSource.setPoolPreparedStatements(true);
+		druidDataSource.setMaxOpenPreparedStatements(20);
+		return druidDataSource;
+	}
+	
+	@Bean
+	public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+		LocalContainerEntityManagerFactoryBean entityManagerFactoryBean = new LocalContainerEntityManagerFactoryBean();
+		entityManagerFactoryBean.setDataSource(getDataSource());
+		entityManagerFactoryBean.setPackagesToScan("com.vito16.shop.model");
+		Properties jpaProperties = new Properties();
+		jpaProperties.setProperty("hibernate.hbm2ddl.auto", "update");
+		jpaProperties.setProperty("hibernate.ejb.naming_strategy", "org.hibernate.cfg.ImprovedNamingStrategy");
+		
+		entityManagerFactoryBean.setJpaProperties(jpaProperties);
+		entityManagerFactoryBean.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+		
+		return entityManagerFactoryBean;
+	}
 
 }
