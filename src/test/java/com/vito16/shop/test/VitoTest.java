db53ee7d0561e0597e7828e2826e269ab18fc77b
Vito <zhouwentao16@gmail.com>
2018-02-09 15:27:56 +0800

    修改依赖的软件版本

---
+++ src/test/java/com/vito16/shop/test/VitoTest.java
@@ -0,0 +1,24 @@
+package com.vito16.shop.test;
+
+import com.google.common.util.concurrent.RateLimiter;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.BlockJUnit4ClassRunner;
+
+/**
+ * @author 木鱼 muyu@yiji.com
+ * @version 2017/12/25
+ */
+@RunWith(BlockJUnit4ClassRunner.class)
+public class VitoTest {
+
+
+    @Test
+    public void t1(){
+        RateLimiter limiter = RateLimiter.create(100);
+
+        System.out.println(limiter.acquire(10));
+        System.out.println(limiter.acquire(20));
+        System.out.println(limiter.acquire(30));
+    }
+}
