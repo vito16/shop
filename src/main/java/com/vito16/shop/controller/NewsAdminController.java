732753c11216337e67ba8ea67a01d27d1a273a39
vito <zhouwentao16@gmail.com>
2019-09-26 23:08:20 +0800

    修改几个管理后台页面以及controller

---
+++ src/main/java/com/vito16/shop/controller/NewsAdminController.java
@@ -0,0 +1,45 @@
+package com.vito16.shop.controller;
+
+import com.vito16.shop.common.Page;
+import com.vito16.shop.model.News;
+import com.vito16.shop.service.NewsService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author Vito zhouwentao16@gmail.com
+ * @date 2013-7-8
+ */
+@Controller
+@RequestMapping("/admin/news")
+public class NewsAdminController {
+    private static final Logger logger = LoggerFactory.getLogger(NewsAdminController.class);
+    @Autowired
+    NewsService newsService;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ModelAndView index(ModelAndView model, HttpServletRequest request) {
+        Page<News> page = new Page<News>(request);
+        newsService.findNews(page);
+        model.addObject("page", page);
+        model.setViewName("admin/news/newsAdmin");
+        return model;
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public ModelAndView viewNews(@PathVariable Integer id,ModelAndView model, HttpServletRequest request) {
+        News news = newsService.findById(id);
+        model.addObject("news", news);
+        model.setViewName("news/newsDetail");
+        return model;
+    }
+
+}
